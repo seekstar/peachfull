64a7d972f7d3e6a2e407ddd02611ff7111dacf3b
searchstar <632863986@qq.com>
2020-07-04 18:07:33 +0800

    Some clean up

--- src/renderer/views/shop/index.vue
+++ src/renderer/views/shop/index.vue
@@ -149,7 +149,7 @@
         </el-col>
       </table>
     </el-dialog>
-    <el-dialog :visible.sync='checkPayVisible'>
+    <el-dialog title='请在弹出的网页支付' :visible.sync='checkPayVisible'>
       <table>
         <el-button @click='checkPay'>我已完成付款</el-button>
         <span>{{checkRes}}</span>
@@ -215,8 +215,6 @@ export default {
     },
     async onBuy() {
       await addOrder(this.token, [this.item], this.consignee)
-      console.log('DEBUG: onBuy')
-      console.log(this.item)
       const alipaySdk = new AlipaySdk({
         // appId: '2021001170648180',
         appId: '2016102900776137',
@@ -224,6 +222,7 @@ export default {
         privateKey: 'MIIEpAIBAAKCAQEA5++VlS85bpoJUp8xSOaoQIvnyMPfjdvohIf+S3P5Rt0pqiHEGXzcrZtMtCUioFUzDhFvjaLuEa8IFS4XR2P5YahDMafE5cohuofaDe1vmF5qLfBSE9ifRNnk11Bk2Fmh2Eb2FI8sb5lKv357WkDeV54W/zeqDzJKDS33lEYnYUUV0ED6FU8Cd+IxD3X3FMun/DL5PvMNzztmwfVMnxEAZTdcr8cRvR3vwfAzZF6CcI7QAiKtYeD5BY+HC/yZXtvZ8jyVEhNVKX6EmW3xQzQMOQzYBk3ei99bFwyv/pS8eFEFk0fyLpkXN0IexNamo5KlXWG4MpCHYoAMCDsrl6KlHQIDAQABAoIBAEhPGYN5RZ6Fx4JKv0LLJol5FLoy1u+TL9qhy1YawgylxueTv19y3hoB4NgI+9KgvVoVdYHi2fYJa6uuwvMu6ADaRJimhWxeqEVPNVxCOhh21wObePtW/dCpXf8CwW3yA9M5zF58hI4GbJdnB24L60XFs8W9xPcX5p5VRmqjtINOPpGg892P2uIN/8knZ/j0kA4TuMUPay7nm6oWfrjmboCH5ARkcTza/nhfRfAINfSXfCCHDI08EF45yNkrRRBiD6X7HIJQO7hmMoGRW/WrPHVQRziILxaj0wnXzSPyltcrvFRTzitVwX9DFa0n80HWVvIQ8JpZM+Tb0HF4XzxHTAECgYEA+hVp9RWkMkjgse9V/WyIvMZ5leaVld17L0JZqlC28pr9ze6NV0z5199GXBkk0Xc13nJRZ3RIYosSwM3uTu3sdDaMCpQbTL9A9bpKASWM/N2QYaJzZCjGj9WFBZA26gcLaawwCCpAVgUAHEQju1PtvEnEWdNWODgeudaK2eRIwQ0CgYEA7WxDAQIGuRsMTIR4XkniLe7pNu5qGFdapYkAwsotdPHWd/KitDUMsmfNvwuzGdwcZIf1mER99JIEAL4avfdcc92iehX3QXDU+gaVoqV7IWSMV87pavc1I7FBnUDc/DfnvsiezI8FzMK9PKLO+tM7jcHsuKR3rM8rh1pGD7AP0FECgYEAupU/eJus5dd5zUfD4FAZtL5f+HcRI78bKSdXvrp5xIfe1MYmrfvAbE8pL42S6rUGe/DOy2D2oI8ibrc2Fa1tvUb1NO9LbvdCgBN9I13g0sh78FxDu3awrp+fn4b5caHP4geVzoG3fnY8nto3zovP06s1087ZhRhZ1E09S1QG9AECgYAHOUYIn9EojKQhJ96pr8HL9/rsivB7cRz28GYvhws/BaSq3Z0JspuCSho+d9KpNMNAY8qbKWpTqKWqiB40LXSIqfOpl0WAnjg8qzqeSj6m03JZsZf0gzUAswH2EOUoX2MTsPLz8l4QztQHJkT56LdlJsNXEtn5Tgxa/+olzAuPIQKBgQDNYpLP5a/u5ffwceBRre2kISXMvf3qyLZK87rgim4HzjhWD+BRw//XCT4G6tdho4Wbb0ZxsSIZfNZHr8WPzyZFgluyhtEssmz0050teZXtn8crDScKH/uTpN+Ir/UVrRv3VRGTobERo+UhFYT6s5TLt+IL1LSXcE0VXimuxUxjyQ=='
       })
       this.lastOutTradeNo = 'peachfull_' + (new Date()).valueOf()
+      console.log(this.lastOutTradeNo)
       const formData = new AlipayFormData()
       formData.setMethod('get')
       formData.addField('notifyUrl', 'http://www.com/notify') // TODO
@@ -237,7 +236,6 @@ export default {
       const result = await alipaySdk.exec('alipay.trade.page.pay', {}, { formData: formData })
       console.log(result)
       shell.openExternal(result)
-      this.$message.success('请在弹出的网页支付')
       this.buyVisible = false
       this.detailVisible = false
       this.checkPayVisible = true
@@ -274,6 +272,8 @@ export default {
                 _this.$message.success('支付成功')
                 // TODO: Make the seller know that
                 _this.checkPayVisible = false
+                _this.lastOutTradeNo = null
+                _this.checkRes = ''
               } else {
                 _this.checkRes = obj.alipay_trade_query_response.msg + ':' + obj.alipay_trade_query_response.sub_msg
               }
