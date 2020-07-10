860f000cfa8d5314b0b08baf3a7d555caf1b0a63
searchstar <632863986@qq.com>
2020-07-10 15:20:03 +0800

    Use request in comment API

--- src/renderer/api/comment.js
+++ src/renderer/api/comment.js
@@ -1,12 +1,17 @@
 import { query } from '@/utils/mysql'
 
 export async function getComment(goods_no) {
-  const result = await query('select * from comment where goods_no=?', goods_no)
-  return result
+  return request({
+    url: '/comment',
+    method: 'get',
+    data: {goods_no: goods_no}
+  })
 }
 
 export async function addComment(buyer, order_no, rating, comment) {
-  const result = await query('select goods_no from `order` where order_no=?', [order_no])
-  const { goods_no } = result[0]
-  await query('insert into comment set ?', { goods_no, order_no, rating, comment, buyer })
+  return request({
+    url: '/comment',
+    method: 'post',
+    data: {buyer: buyer, order_no: order_no, rating: rating, comment: comment}
+  })
 }
